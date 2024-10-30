1d8c58d859ef0b431197c7f46d0a977b705ad7c4
zu2220 <u22206853@utp.edu.pe>
2024-10-29 21:38:49 -0500

    he modificado api.js

--- cliente/src/api.js
+++ cliente/src/api.js
@@ -11,3 +11,5 @@ const API_URL = 'http://localhost:3000/api/auth';
     const response = await axios.post(`${API_URL}/register`, user);
     return response.data;
   };
+
+  //este es un comentario de prueba
